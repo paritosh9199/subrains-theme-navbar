724f3a22882efa4a3b7a9b21164c7db3a584df2e
paritosh9199 <paritoshsrivastava9199@gmail.com>
2018-09-27 19:33:56 +0530

    initial commit

---
+++ style.js
@@ -0,0 +1,13 @@
+function _(query){
+  return document.querySelector(query);
+}
+
+// alert("connected"+screen.width);
+if(screen.width <= 400){
+  
+  document.getElementById("min-style").innerHTML = "<br><br><br>";
+  
+} else {
+  
+  
+}
